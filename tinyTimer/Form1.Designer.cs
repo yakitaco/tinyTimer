c60f25b7612f9490853121eeb750f2441f4d3fb8
yaki_taco <yaki_taco@yahoo.co.jp>
2021-09-28 21:23:00 +0900

    [tinyTimer]First release.

---
+++ tinyTimer/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace tinyTimer {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Stop";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 60);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 39);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(125, 60);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 39);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(238, 60);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(107, 39);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Reset";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.numericUpDown1.Location = new System.Drawing.Point(351, 63);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(74, 28);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 105);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(410, 103);
+            this.textBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 220);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "[STOP} tinyTimer";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ tinyTimer/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace tinyTimer {
+
+    public partial class Form1 : Form {
+        static int cnt = 0;
+        private static System.Threading.Timer tm;
+        static Form1 Instance;
+        DateTime beforeTime;
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            Instance = this;
+
+        }
+
+        delegate void delegate1(String text1);
+        public void addMsg(string msg) {
+            Invoke(new delegate1(FormAddMsg), msg);
+        }
+        public void FormAddMsg(string msg) {
+            label1.Text = msg;
+            textBox1.AppendText(msg + "\r\n");
+        }
+
+        // 指定秒数間隔で呼び出される処理
+        TimerCallback callback = state => {
+            DateTime newTime = DateTime.Now;
+            TimeSpan diff = newTime - Instance.beforeTime;
+
+            if ((int)diff.TotalSeconds != (int)Instance.numericUpDown1.Value) {
+                Instance.addMsg($"[{++cnt}] " + newTime + " *" + (int)diff.TotalSeconds);
+            } else {
+                Instance.addMsg($"[{++cnt}] " + newTime);
+            }
+            Instance.beforeTime = newTime;
+        };
+
+        private void button1_Click(object sender, EventArgs e) {
+            this.Text = "[START] tinyTimer";
+            button1.Enabled = false;
+            numericUpDown1.Enabled = false;
+            button2.Enabled = true;
+            beforeTime = DateTime.Now;
+            beforeTime = beforeTime.AddSeconds(-(int)numericUpDown1.Value);
+            // タイマー起動
+            tm = new System.Threading.Timer(callback, null, 0, (int)numericUpDown1.Value * 1000);
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            this.Text = "[STOP] tinyTimer";
+            tm.Dispose();
+            button2.Enabled = false;
+            numericUpDown1.Enabled = true;
+            button1.Enabled = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e) {
+            cnt = 0;
+            textBox1.Text = "";
+        }
+    }
+}
